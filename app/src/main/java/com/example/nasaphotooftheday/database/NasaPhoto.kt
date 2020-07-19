62bd647e7afc4aef4ac118967ebf857717c2196a
Neeti <neeti@playo.co>
2020-07-19 23:16:00 +0530

    Create data class

---
+++ app/src/main/java/com/example/nasaphotooftheday/database/NasaPhoto.kt
@@ -0,0 +1,32 @@
+package com.example.nasaphotooftheday.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
+@Entity(tableName = "npod")
+data class NasaPhoto(
+
+    @PrimaryKey
+    @SerializedName("date")
+    val date : Date,
+
+    @SerializedName("explanation")
+    val description : String?,
+
+    @SerializedName("hdurl")
+    val hdurl : String?,
+
+    @SerializedName("media_type")
+    val media_type : String?,
+
+    @SerializedName("title")
+    val title : String?,
+
+    @SerializedName("url")
+    val url : String?,
+
+    @SerializedName("thumbnail_url")
+    val thumbnail_url : String?
+)
