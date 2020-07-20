43eea088460f37731c3ef10f4a30418d31abd662
Neeti <neeti@playo.co>
2020-07-20 08:40:19 +0530

    Create Network Datasource

---
+++ app/src/main/java/com/example/nasaphotooftheday/service/NasaPhotoNetworkDataSource.kt
@@ -0,0 +1,14 @@
+package com.example.nasaphotooftheday.service
+
+import com.example.nasaphotooftheday.database.entity.NasaPhoto
+import io.reactivex.Single
+
+class NasaPhotoNetworkDatasource(private val apiService: NasaPhotoService) {
+    fun getNasaPhotoDetails() : Single<NasaPhoto> {
+        return apiService.getNasaPhoto()
+    }
+
+    fun getNasaPhotoOnDateDetails(date:String) : Single<NasaPhoto>{
+        return apiService.getNasaPhotoOnDate(date)
+    }
+}
